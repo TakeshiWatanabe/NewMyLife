e13b3f32f54de128518114e1cec4b34a3839aa0c
渡邉　剛志 <takeshi.w9@icloud.com>
2015-09-23 14:01:22 +0900

    Hmm

--- hobby.php
+++ hobby.php
@@ -29,95 +29,110 @@ include("titleAndButton.php");
         </figcaption>
       </div>
     </div>
-    <div class="col-md-4 col-md-offset-2 well">
-      <li>I like music</li>
-    </div>
-  </div>
-  <div style="margin-top:50px">
-    <div class="col-md-4 col-md-offset-2 well">
-      <li>I like music</li>
-    </div>
-  </div>
-  <div style="margin-top:40px">
-    <div class="col-md-3 col-md-offset-1">
-      <img src="hobbyImg/IMG_0747.JPG" width="400" height="400">
-      <figcaption>
-        <div style="margin-left:0px"><font size="4">Stanford University</font></div>
-      </figcaption>
-    </div>
-  </div>
-  <div style="margin-top:440px">
-    <div class="row">
-      <div style="margin-top:90px">
-        <div class="col-md-2 col-md-offset-2">
-          <img src="hobbyImg/snowbord.jpg" width="400" height="400">
-          <figcaption>
-            <div style="margin-left:0px"><font size="4">Niigata</font></div>
-          </figcaption>
-        </div>
-      </div>
+    <div style="margin-top:150px">
       <div class="col-md-4 col-md-offset-2 well">
         <li>I like music</li>
       </div>
     </div>
   </div>
-  <div style="margin-top:50px">
-    <div class="col-md-4 col-md-offset-2 well">
-      <li>I like music</li>
-    </div>
-  </div>
-  <div style="margin-top:40px">
-    <div class="col-md-3 col-md-offset-1">
-      <img src="hobbyImg/drink.jpg" width="400" height="400">
-      <figcaption>
-        <div style="margin-left:0px"><font size="4">Shibuya</font></div>
-      </figcaption>
-    </div>
-  </div>
-  <div style="margin-top:440px">
-    <div class="row">
-      <div style="margin-top:90px">
-        <div class="col-md-2 col-md-offset-2">
-          <img src="hobbyImg/gym.jpg" width="400" height="400">
-          <figcaption>
-            <div style="margin-left:0px"><font size="4">Shimokitzawa</font></div>
-          </figcaption>
-        </div>
+
+  <div class="row">
+    <div style="margin-top:40px">
+      <div class="col-md-2 col-md-offset-2">
+        <img src="hobbyImg/IMG_0747.JPG" width="400" height="400">
+        <figcaption>
+          <div style="margin-left:0px"><font size="4">Stanford University</font></div>
+        </figcaption>
       </div>
+    </div>
+    <div style="margin-top:150px">
       <div class="col-md-4 col-md-offset-2 well">
-        <li>I like music</li>
+        <li>I like travel</li>
       </div>
     </div>
   </div>
-  <div style="margin-top:50px">
-    <div class="col-md-4 col-md-offset-2 well">
-      <li>I like music</li>
-    </div>
-  </div>
-  <div style="margin-top:40px">
-    <div class="col-md-3 col-md-offset-1">
-      <img src="hobbyImg/IMG_1054.JPG" width="400" height="400">
-      <figcaption>
-        <div style="margin-left:0px"><font size="4">Chiba</font></div>
-      </figcaption>
-    </div>
-  </div>
-  <div style="margin-top:440px">
-    <div class="row">
-      <div style="margin-top:90px">
-        <div class="col-md-2 col-md-offset-2">
-          <img src="hobbyImg/ageha.jpg" width="400" height="400">
-          <figcaption>
-            <div style="margin-left:0px"><font size="4">Shinkiba ageha</font></div>
-          </figcaption>
-        </div>
+
+  <div class="row">
+    <div style="margin-top:40px">
+      <div class="col-md-2 col-md-offset-2">
+        <img src="hobbyImg/snowbord.jpg" width="400" height="400">
+        <figcaption>
+          <div style="margin-left:0px"><font size="4">Niigata</font></div>
+        </figcaption>
       </div>
+    </div>
+    <div style="margin-top:150px">
       <div class="col-md-4 col-md-offset-2 well">
-        <li>I like music</li>
+        <li>I like snowboard</li>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div style="margin-top:40px">
+      <div class="col-md-2 col-md-offset-2">
+        <img src="hobbyImg/drink.jpg" width="400" height="400">
+        <figcaption>
+          <div style="margin-left:0px"><font size="4">Shibuya</font></div>
+        </figcaption>
+      </div>
+    </div>
+    <div style="margin-top:150px">
+      <div class="col-md-4 col-md-offset-2 well">
+        <li>I like drink</li>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div style="margin-top:40px">
+      <div class="col-md-2 col-md-offset-2">
+        <img src="hobbyImg/gym.jpg" width="400" height="400">
+        <figcaption>
+          <div style="margin-left:0px"><font size="4">Shimokizawa</font></div>
+        </figcaption>
+      </div>
+    </div>
+    <div style="margin-top:150px">
+      <div class="col-md-4 col-md-offset-2 well">
+        <li>I like gym</li>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div style="margin-top:40px">
+      <div class="col-md-2 col-md-offset-2">
+        <img src="hobbyImg/IMG_1054.JPG" width="400" height="400">
+        <figcaption>
+          <div style="margin-left:0px"><font size="4">Chiba</font></div>
+        </figcaption>
+      </div>
+    </div>
+    <div style="margin-top:150px">
+      <div class="col-md-4 col-md-offset-2 well">
+        <li>I like cycling</li>
       </div>
     </div>
   </div>
   
+  <div class="row">
+    <div style="margin-top:40px">
+      <div class="col-md-2 col-md-offset-2">
+        <img src="hobbyImg/ageha.jpg" width="400" height="400">
+        <figcaption>
+          <div style="margin-left:0px"><font size="4">Shinkiba ageha</font></div>
+        </figcaption>
+      </div>
+    </div>
+    <div style="margin-top:150px">
+      <div class="col-md-4 col-md-offset-2 well">
+        <li>I like party</li>
+      </div>
+    </div>
+  </div>
+
+
   <?php
   // footer
   include("footer.php");
